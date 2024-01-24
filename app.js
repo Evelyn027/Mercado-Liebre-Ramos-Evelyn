42a46a88f9486929aa883471c0d1d2120a227705
Evelyn Ramos <eveh.rms7@gmail.com>
2024-01-24 11:04:36 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const { log } = require("console");
+const express = require("express");
+const path = require("path");
+const { isDeepStrictEqual } = require("util");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+/* cONFIGURACION DE SERVIDOR*/
+/* app.listen(3500, () => {
+    console.log('Servidor corriendo');
+}); */
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log("Servidor corriendo en el puerto" + port);
+});
+
+/* ROUTES */
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views", "home.html"));
+});
