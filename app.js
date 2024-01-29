81db4ce9f5381b99572dc5553f11a98fb1730ff8
Evelyn Ramos <eveh.rms7@gmail.com>
2024-01-29 20:15:30 -0300

    css arreglado

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use('/public',express.static('public'));
+app.use(express.static('public'));
 
 /* CONFIGURACION DE SERVIDOR*/
 
