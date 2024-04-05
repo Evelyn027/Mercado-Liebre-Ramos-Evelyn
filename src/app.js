e5d56e984c38a570a34ed9168b667091064b3149
Evelyn027 <eveh.rms7@gmail.com>
2024-04-05 15:38:03 -0300

    Ejs

--- src/app.js
+++ src/app.js
@@ -5,28 +5,19 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
+// Indicamos el motor de plantilla
+app.set('view engine', 'ejs')
+//Direccionamos a las vistas
+app.set('views', path.join(__dirname, '/views'))
+
 /*-----ROUTES-----*/
-/* app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/views/home.html`)
-}); */
-const homeRouter = require('./routes/homeRoutes.js');
-app.use('/', homeRouter);
+const homeRouter = require('./routes/homeRoutes')
 
-/* app.get('/register', (req, res) => {
-    // res.sendFile(path.join(__dirname, '/views/register.html'))
-    res.sendFile(`${__dirname}/views/register.html`)
-}); */
 
-/* app.get('/login', (req, res) => {
-    // res.sendFile(path.join(__dirname, '/views/login.html'))
-    res.sendFile(`${__dirname}/views/login.html`)
-}); */
+app.use('/', homeRouter)
 
 
 /*-----CONFIGURACION DE SERVIDOR-----*/
-/* app.listen(port, () => {
-    console.log("Servidor corriendo en el puerto " + port);
-});  */
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 });
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,17 @@
 const path = require('node:path');
+const data = require('../model/products.json')
 
 const controller = {
     renderHome: (req, res) =>{
-        const pathHome = path.join(__dirname, '../views/home.html');
-        res.sendFile(pathHome);
+        /* const pathHome = path.join(__dirname, '../views/home.html');
+        res.sendFile(pathHome); */
+        res.render('home.ejs', { products: data.products });
     }
-}
+} 
+
+/* class HomeController {
+    static renderHome(req, res) {
+        res.render('home.ejs', { products: data.products })
+    }
+    } */
 module.exports = controller;
